046842fb7b4ac71984a97a45fc24971ca05dda24
Evan Kahn <ekahn@hmc.edu>
2020-03-25 10:38:13 -0400

    updates timestamps correctly, updates file contents after each write, closes file after done

--- eodreport.py
+++ eodreport.py
@@ -7,18 +7,21 @@ if not os.path.exists(reportStorage):
 today = datetime.date.today()
 strToday = today.strftime("%x")
 strToday = strToday.replace("/", "-")
-now = datetime.datetime.now()
 todaysReport = reportStorage + "/" + strToday + ".txt"
 if not os.path.exists(todaysReport):
     f = open(reportStorage + "/" + strToday + ".txt", "x")
 else:
     f = open(reportStorage + "/" + strToday + ".txt", "a")
-current_time = now.strftime("%H:%M:%S")
 print("Welcome! Type /quit to exit. \n")
 while True:
     userInput = input("What are you working on?: ")
     if userInput == "/quit":
         break
     else:
+        now = datetime.datetime.now()
+        current_time = now.strftime("%H:%M:%S")
         f.write("[" + current_time + "]" + "\n" + userInput + "\n" + "\n")
+        f.flush()
+
+f.close()
     
